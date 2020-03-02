268e7efdbbfd48d19260981c99cc10ac19fce970
cvorak <cvorak@gmail.com>
2020-03-02 19:59:50 +0100

    exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,10 +6,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [count, setCount] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+      setGood(good + 1)
+      setCount(count + 1)
+  }
+  const handleNeutral = () => {
+      setNeutral(neutral + 1)
+      setCount(count + 1)
+  }
+  const handleBad = () => {
+      setBad(bad + 1)
+      setCount(count + 1)
+  }
+
+  const getAverage = () => (good - bad) / count
+
+  const getPositive = () => good / count * 100
 
   return (
     <div>
@@ -21,6 +35,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {count}</div>
+      <div>average {getAverage()}</div>
+      <div>positive {getPositive()}</div>
     </div>
   )
 }
