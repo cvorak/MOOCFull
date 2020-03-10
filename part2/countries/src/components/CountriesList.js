3242d8bf8210da773bb985422355eaf8da23008f
cvorak <cvorak@gmail.com>
2020-03-10 19:49:07 +0100

    exercise 2.13

--- part2/countries/src/components/CountriesList.js
+++ part2/countries/src/components/CountriesList.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CountryWithFlag from './CountryWithFlag'
 
 const CountriesList = ({ countries }) => {
-    const rows = () => countries.map(c => <li key={c.name}>{c.name}</li>)
+    const [countryToShow, setCountryToShow] = useState('')
+
+    const handleShow = (event) => {
+        setCountryToShow(event.target.getAttribute('data-name'))
+    }
+
+    const rows = () => countries.map(c => {
+        return (
+            <li key={c.name}>{c.name}
+                <button data-name={c.name} onClick={handleShow}>show</button>
+            </li>
+        )
+    })
+
+    if (countryToShow) return <CountryWithFlag country={countries.find(c => c.name === countryToShow)} />
 
     return (
         <ul>{rows()}</ul>
