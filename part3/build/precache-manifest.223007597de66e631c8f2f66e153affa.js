48d3ae09d779f07d5376a88f99402d4d96e2fb5e
cvorak <cvorak@gmail.com>
2020-04-15 19:47:30 +0200

    exercise 3.9

---
+++ part3/build/precache-manifest.223007597de66e631c8f2f66e153affa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c25c93b65a6a72c5b8aa8013a83349",
+    "url": "/index.html"
+  },
+  {
+    "revision": "707464e413f403b0a134",
+    "url": "/static/css/main.13af8ac1.chunk.css"
+  },
+  {
+    "revision": "8072acd03b9a616ad1b0",
+    "url": "/static/js/2.197f1d1a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.197f1d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707464e413f403b0a134",
+    "url": "/static/js/main.1b1ca108.chunk.js"
+  },
+  {
+    "revision": "ad9c1ae9b2cdeb3f7618",
+    "url": "/static/js/runtime-main.584b4e6a.js"
+  }
+]);
