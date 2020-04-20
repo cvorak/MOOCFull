657d09c56d80d42d59ff2c705c75d852fcb55924
cvorak <cvorak@gmail.com>
2020-04-20 16:28:10 +0200

    exercise 3.17

--- part3/index.js
+++ part3/index.js
@@ -92,6 +92,21 @@ app.post('/api/persons', (req, res) => {
     })
 })
 
+app.put('/api/persons/:id', (req, res) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+        .then(updatedPerson => {
+            res.json(updatedPerson.toJSON())
+        })
+        .catch(err => next(err))
+})
+
 const errorHandler = (err, req, res, next) => {
     console.log(err.message)
 
