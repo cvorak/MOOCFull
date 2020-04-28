19e4a4e65588d64a806ebfd4fcd6bd15d3b85fc4
cvorak <cvorak@gmail.com>
2020-04-28 21:31:14 +0200

    exercise 4.2 extract blogsRouter

--- part4/index.js
+++ part4/index.js
@@ -6,32 +6,15 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const Blog = require('./models/blog')
-
-
-
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
