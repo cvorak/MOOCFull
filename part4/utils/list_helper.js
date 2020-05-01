4a6d02443cb22b3b8ccfb0ba52c181af4e62e28e
cvorak <cvorak@gmail.com>
2020-05-01 15:16:33 +0200

    exercise 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,30 @@ const totalLikes = (blogList) => {
         .reduce((acc, item) => acc + item, 0)
 }
 
+const favoriteBlog = (blogList) => {
+    let favorite
+
+    if (blogList.length === 0) {
+        return null
+    }
+
+    if (blogList.length === 1) {
+        favorite = blogList[0]
+    } else {
+        favorite = blogList.reduce((favorite, current) => {
+            if (favorite.likes > current.likes) {
+                return favorite
+            } else {
+                return current
+            }
+        })
+    }    
+
+    return (({author, likes, title}) => ({author, likes, title}))(favorite)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
