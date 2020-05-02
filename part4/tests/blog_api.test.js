03bd6f78fc85b804f3b15f81d6e97d24d0783499
cvorak <cvorak@gmail.com>
2020-05-02 14:01:20 +0200

    exercise 4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObject = helper.initialBlogs.map(b => Blog(b))
+    const promiseArray = blogObject.map(b => b.save())
+    await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(b => b.title)
+
+    expect(titles).toContain('TDD harms architecture')
+})
+
+
+
+
+afterAll(() => {
+    mongoose.connection.close()
+})
