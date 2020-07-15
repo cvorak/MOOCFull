e05f420aba90a5022ae68977e3c089b6a05b3366
cvorak <cvorak@gmail.com>
2020-07-15 19:52:09 +0200

    exercise 4.15

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const logger = require('./utils/logger')
 const config = require('./utils/config')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const app = express()
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -18,5 +19,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
