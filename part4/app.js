424c581d4fc9040b4848928a9848b125318f7e5a
cvorak <cvorak@gmail.com>
2020-07-18 19:15:31 +0200

    exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -7,6 +7,8 @@ const cors = require('cors')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginrouter = require('./controllers/login')
+const loginRouter = require('./controllers/login')
 const app = express()
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,6 +24,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 app.use(middleware.unknownEndpoint)
