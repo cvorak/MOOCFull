4755d8214006f04abda1922478322782da8d4d75
cvorak <cvorak@gmail.com>
2020-07-18 19:01:27 +0200

    exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,9 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', {username: 1, name: 1})
+        
     response.json(blogs)
 })
 
@@ -11,8 +15,22 @@ blogsRouter.post('/', async (request, response) => {
     if (!request.body.title && !request.body.url) {
         response.status(400).json( {error: 'url and title missing'})
     } else {
-        const blog = new Blog(request.body)
+        const body = request.body
+        const user = await User.findOne({})
+
+        const blog = new Blog({
+            title: body.title,
+            likes: body.likes,
+            author: body.author,
+            url: body.url,
+            user: user._id
+        })
+
         const savedBlog = await blog.save()
+
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
         response
             .status(201)
             .json(savedBlog.toJSON())
