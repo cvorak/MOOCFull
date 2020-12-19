012eab5cf7a2c27125a66c72edddf40970b8fd1f
Nikola Čvorović <cvorak@gmail.com>
2020-12-19 18:29:16 +0100

    exercise 5.2

--- part5/src/App.js
+++ part5/src/App.js
@@ -15,17 +15,30 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const user = window.localStorage.getItem('loggedInUser')
+    if (user) {
+      setUser(JSON.parse(user))
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedInUser', JSON.stringify(user))
       setUser(user)
       setUsername('')
       setPassword('')
     }
     catch (exception) {
-
+      
     }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedInUser')
   }
 
   const loginForm = () => (
@@ -54,7 +67,9 @@ const App = () => {
   const blogList = () => (
     <>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      {user.name} logged in
+      <button onClick={handleLogout}>log out</button>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
