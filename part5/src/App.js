c722d8fd020c50aa60aaea9f1591a5aa4b045135
Nikola Čvorović <cvorak@gmail.com>
2020-12-21 16:48:55 +0100

    exercise 5.4

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -11,6 +12,8 @@ const App = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [url, setUrl] = useState('')
+  const [message, setMessage] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
@@ -36,8 +39,11 @@ const App = () => {
       setPassword('')
     }
     catch (exception) {
-      
-    }
+      setMessage('Wrong credentials')
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    } 
   }
 
   const handleLogout = () => {
@@ -70,60 +76,68 @@ const App = () => {
 
   const handleCreateNew = async (event) => {
     event.preventDefault()
+    try {
+      const newBlog = {
+        title: title,
+        url: url,
+        author: author
+      }
 
-    const newBlog = {
-      title: title,
-      url: url,
-      author: author
+      const returnedBlog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(returnedBlog))
+      setUrl('')
+      setAuthor('')
+      setTitle('')
+      setMessage(`a new blog ${title} by ${author} added`)
+      setTimeout(() => {setMessage('')}, 5000)
     }
-
-    const returnedBlog = await blogService.create(newBlog)
-    setBlogs(blogs.concat(returnedBlog))
-    setUrl('')
-    setAuthor('')
-    setTitle('')
-        
+    catch (exception) {
+      setMessage(exception)
+      setTimeout(() => {
+        setMessage(null)
+      }, 5000)
+    }
   }
 
-  const createNew = () =>  (
+  const createNew = () => (
     <>
-    <h2>create new</h2>
-    <form onSubmit={handleCreateNew}>
-      title:  
-      <input 
-      type='text'
-      name='Title'
-      value={title}
-      onChange={({target}) => setTitle(target.value)}
-      />
-      <br></br>
-      author:  
-      <input 
-      type='text'
-      name='Author'
-      value={author}
-      onChange={({target}) => setAuthor(target.value)}
-      />
-      <br></br>
-      url:  
-      <input 
-      type='text'
-      name='Url'
-      value={url}
-      onChange={({target}) => setUrl(target.value)}
-      />  
-      <br></br>
-      <button type='submit'>create</button>
-    </form>
+      <h2>create new</h2>
+      <form onSubmit={handleCreateNew}>
+        title:
+      <input
+          type='text'
+          name='Title'
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <br></br>
+      author:
+      <input
+          type='text'
+          name='Author'
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+        <br></br>
+      url:
+      <input
+          type='text'
+          name='Url'
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+        />
+        <br></br>
+        <button type='submit'>create</button>
+      </form>
     </>
   )
 
   const blogList = () => (
     <>
-    <h2>blogs</h2>
-    {user.name} logged in       <button onClick={handleLogout}>log out</button>
-    {createNew()}   
-      
+      <h2>blogs</h2>
+      {user.name} logged in       <button onClick={handleLogout}>log out</button>
+      {createNew()}
+
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
@@ -131,10 +145,11 @@ const App = () => {
     </>
   )
 
-  
+
 
   return (
     <div>
+      <Notification message={message} />
       {user ?
         blogList() :
         loginForm()
