07c863d10265847d76eb5a53fa79b5641cef747f
Nikola Čvorović <cvorak@gmail.com>
2020-12-29 08:32:04 +0100

    exercise 5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+  const [buttonName, setButtonName] = useState('view')
+
+  const showWhenVisible = {display : visible ? '' : 'none'} 
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+    visible ? setButtonName('show') : setButtonName('hide')
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisible}>{buttonName}</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br/>
+        likes {blog.likes}
+        <button>like</button>
+        <br/>
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
