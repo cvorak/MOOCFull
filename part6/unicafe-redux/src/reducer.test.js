d338b826307a5edb48bbb33144d9b5a8a5317719
Nikola Čvorović <cvorak@gmail.com>
2021-01-23 10:02:49 +0100

    exercise6.1

--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,48 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('stats are resetted', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {
+      good: 4,
+      bad: 3,
+      ok: 9
+    }
+
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual(initialState)
+  })
 })
