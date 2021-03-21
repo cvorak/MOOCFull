3eddda900b3fe5ae26ba7bc426ff96b724f31ee8
Nikola Cvorovic <nikola.cvorovic@sap.com>
2021-03-21 16:49:32 +0100

    exercise 7.4

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -8,6 +8,8 @@ import {
   useHistory,
 } from "react-router-dom";
 
+import {useField} from './hooks/index'
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
@@ -60,20 +62,20 @@ const Footer = () => (
 const CreateNew = (props) => {
   const history = useHistory();
 
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     history.push("/");
-    props.setNotification(`a new anecdote ${content} created`)
+    props.setNotification(`a new anecdote ${content.value} created`)
     setTimeout(() => props.setNotification(null), 10000)
   };
 
@@ -83,27 +85,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
